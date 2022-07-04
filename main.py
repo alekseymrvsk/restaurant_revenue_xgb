6bb5c6e7e7b67fcc0cf9732e0da30767b05aa106
Aleksey <alekseymrvsk@yandex.by>
2022-07-04 14:29:56 +0300

    XGB

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import numpy as np
-import os
 import pandas as pd
-import sklearn
-from sklearn.preprocessing import MinMaxScaler, LabelBinarizer
-from sklearn import datasets
-from sklearn import metrics
 from sklearn.model_selection import train_test_split, RepeatedKFold, cross_val_score
-import matplotlib.pyplot as plt
-import seaborn as sns
 import xgboost as xgb
 
-plt.style.use("ggplot")
 
 inputPathTrain = 'input/train.csv'
 inputPathTest = 'input/train.csv'
@@ -25,7 +16,7 @@ x_train = data_train.drop(columns=['revenue', 'Id', 'Open Date', 'City', 'City G
 x_test = data_test.drop(columns=['revenue', 'Id', 'Open Date', 'City', 'City Group', 'Type'])
 x_test = x_test.astype(int)
 
-model = xgb.XGBRegressor(n_estimators=1000, max_depth=7, eta=0.1, subsample=0.7, colsample_bytree=0.8)
+model = xgb.XGBRegressor(n_estimators=1300, max_depth=7, eta=0.1, subsample=0.7, colsample_bytree=0.8)
 model.fit(x_train, y_train)
 
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)

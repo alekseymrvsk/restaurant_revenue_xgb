9734d0305c3fc8f6f171ab0e5be28fd64183d979
Aleksey <alekseymrvsk@yandex.by>
2022-07-04 22:46:38 +0300

    XGB

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split, RepeatedKFold, cross_val_score
-import xgboost as xgb
+'''
+TODO:
+output to file
+constants for params
+'''
 
+import pandas as pd
+from sklearn.model_selection import RepeatedKFold, cross_val_score
+import xgboost as xgb
+import time
+
+GRADIENT_BOOSTED_TREES_COUNT = 1000
 
 inputPathTrain = 'input/train.csv'
 inputPathTest = 'input/train.csv'
+columns_for_hash = ['Open Date', 'City', 'City Group', 'Type']
 
 data_train = pd.read_csv(inputPathTrain)
 data_test = pd.read_csv(inputPathTest)
 
 y_train = data_train.revenue
 y_train = y_train.astype(int)
-x_train = data_train.drop(columns=['revenue', 'Id', 'Open Date', 'City', 'City Group', 'Type'])
+x_train = data_train.drop(columns=['revenue', 'Id'])
 
-x_test = data_test.drop(columns=['revenue', 'Id', 'Open Date', 'City', 'City Group', 'Type'])
-x_test = x_test.astype(int)
+for column in columns_for_hash:
+    x_train[column] = x_train[column].apply(hash)
 
-model = xgb.XGBRegressor(n_estimators=1000, max_depth=8, eta=0.01, subsample=0.7, colsample_bytree=0.8)
+x_test = data_test.drop(columns=['revenue', 'Id'])
+for column in columns_for_hash:
+    x_test[column] = x_test[column].apply(hash)
+
+model = xgb.XGBRegressor(n_estimators=GRADIENT_BOOSTED_TREES_COUNT, max_depth=8, eta=0.01, subsample=0.7,
+                         colsample_bytree=0.8)
 model.fit(x_train, y_train)
 
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
@@ -26,8 +40,10 @@ scores = abs(scores)
 
 print('Mean MAE: %.3f (%.3f)' % (scores.mean(), scores.std()))
 
+start_time = time.time()
 prediction = model.predict(x_test)
+end_time = time.time()
 
-print('Prediction: ', prediction)
+print("Time of prediction: ", end_time - start_time)
 
 model.save_model("model_xgb.json")

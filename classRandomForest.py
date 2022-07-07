90faae461c4724b438130276e7e2331f521a20fa
Aleksey <alekseymrvsk@yandex.by>
2022-07-07 12:50:57 +0300

    commit

--- classRandomForest.py
+++ classRandomForest.py
@@ -31,34 +31,25 @@ def dataset_split_test(INPUT_PATH_TEST='input/test.csv'):
 
 
 class MyRandomForest:
-    # def __init__(self, RANDOM_STATE_MODEL=1, TREES_COUNT=50, N_SPLITS=10, N_REPEATS=3, RANDOM_STATE_METRIC=1):
-    #     self.scores = None
-    #     self.model = None
-    #     self.dictionary = AttrDict({"RANDOM_STATE_MODEL": RANDOM_STATE_MODEL,
-    #                                 "TREES_COUNT": TREES_COUNT,
-    #                                 "N_SPLITS": N_SPLITS,
-    #                                 "N_REPEATS": N_REPEATS,
-    #                                 "RANDOM_STATE_METRIC": RANDOM_STATE_METRIC})
-
     def __init__(self, RANDOM_STATE_MODEL=1, TREES_COUNT=50, N_SPLITS=10, N_REPEATS=3, RANDOM_STATE_METRIC=1):
         self.scores = None
         self.model = None
-        self.RANDOM_STATE_METRIC = RANDOM_STATE_METRIC
-        self.N_REPEATS = N_REPEATS
-        self.N_SPLITS = N_SPLITS
-        self.RANDOM_STATE_MODEL = RANDOM_STATE_MODEL
-        self.TREES_COUNT = TREES_COUNT
+        self.dictionary = AttrDict({"RANDOM_STATE_MODEL": RANDOM_STATE_MODEL,
+                                    "TREES_COUNT": TREES_COUNT,
+                                    "N_SPLITS": N_SPLITS,
+                                    "N_REPEATS": N_REPEATS,
+                                    "RANDOM_STATE_METRIC": RANDOM_STATE_METRIC})
 
     def fit_model(self, input_path_train='input/train.csv'):
         dataset = dataset_split_train(input_path_train)
         x_train = dataset[0]
         y_train = dataset[1]
-        model = RandomForestRegressor(n_estimators=self.TREES_COUNT,
-                                      random_state=self.RANDOM_STATE_MODEL)
+        model = RandomForestRegressor(n_estimators=self.dictionary.TREES_COUNT,
+                                      random_state=self.dictionary.RANDOM_STATE_MODEL)
         model.fit(x_train, y_train)
         self.model = model
-        cv = RepeatedKFold(n_splits=self.N_SPLITS, n_repeats=self.N_REPEATS,
-                           random_state=self.RANDOM_STATE_METRIC)
+        cv = RepeatedKFold(n_splits=self.dictionary.N_SPLITS, n_repeats=self.dictionary.N_REPEATS,
+                           random_state=self.dictionary.RANDOM_STATE_METRIC)
         scores = cross_val_score(model, x_train, y_train, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
         self.scores = abs(scores)
 
@@ -78,4 +69,3 @@ class MyRandomForest:
     @staticmethod
     def save_model(model):
         joblib.dump(model, "./model_random_forest.joblib")
-
